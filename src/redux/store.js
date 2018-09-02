b4e2a851486158fb6960f3cecf093921cff2a550
Louis Cospain <elcoosp@gmail.com>
2018-09-02 23:58:57 +0200

    feat: 🎸 Add basic persistence with LS
    Need to enhance with throtelling and compression of satte before json
    serialization (+ before unload ?)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,38 @@
 import { createStore, combineReducers } from "redux"
+import { tryCatchUndef, pipe } from "../utils"
 import exercises from "./ducks/Exercise"
 import programs from "./ducks/Program"
 
-const store = createStore(
-	combineReducers({
-		exercises,
-		programs
-	}),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const loadState = tryCatchUndef(() => {
+	const state = localStorage.getItem("state")
+	return state === null ? undefined : JSON.parse(state)
+})
+
+const saveState = tryCatchUndef(
+	pipe(
+		JSON.stringify,
+		state => localStorage.setItem("state", state)
+	)
 )
 
-export default store
+const configureStore = () => {
+	const rootReducer = combineReducers({
+		exercises,
+		programs
+	})
+
+	const store = createStore(
+		rootReducer,
+		loadState(),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+
+	store.subscribe(
+		pipe(
+			store.getState,
+			saveState
+		)
+	)
+	return store
+}
+export default configureStore
