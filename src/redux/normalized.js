43fd1a5148e7ab22c5baa36d3f6bca09f3618507
Louis Cospain <elcoosp@gmail.com>
2018-09-03 17:03:23 +0200

    refactor: 💡 Extract the reducer refs removing in normalized

--- src/redux/normalized.js
+++ src/redux/normalized.js
@@ -1,3 +1,27 @@
+// Grab a field on the payload corresponding to the id refs of the entity to update.
+// Get every entity concerned and create the updated ones with the updater function
+// And glue together the state
+const updateRefs = (state, payload, payloadEntityRefsKey, updater) => {
+	const update = payload[payloadEntityRefsKey].reduce((acc, id) => {
+		const entity = state.byId[id]
+
+		acc[id] = {
+			...entity,
+			...updater(entity)
+		}
+		return acc
+	}, {})
+
+	return { ...state, byId: { ...state.byId, ...update } }
+}
+
+const removeRefs = (state, payload, payloadEntityRefsKey, idsToRemoveRefsKey) =>
+	normalized.updateRefs(state, payload, payloadEntityRefsKey, entity => ({
+		[idsToRemoveRefsKey]: entity[idsToRemoveRefsKey].filter(
+			id => id !== payload.id
+		)
+	}))
+
 const normalized = {
 	add: (state, payload) => ({
 		byId: { ...state.byId, [payload.id]: payload },
@@ -26,22 +50,8 @@ const normalized = {
 		},
 		allIds: state.allIds
 	}),
-	// Grab a field on the payload corresponding to the id refs of the entity to update.
-	// Get every entity concerned and create the updated ones with the updater function
-	// And glue together the state
-	updateRefs: (state, payload, payloadRefsField, updater) => {
-		const update = payload[payloadRefsField].reduce((acc, id) => {
-			const entity = state.byId[id]
-
-			acc[id] = {
-				...entity,
-				...updater(entity)
-			}
-			return acc
-		}, {})
-
-		return { ...state, byId: { ...state.byId, ...update } }
-	}
+	updateRefs,
+	removeRefs
 }
 
 export default normalized
