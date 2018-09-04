cb57f02b96646f8aec756d1f05c6fb4da03a96c8
Louis Cospain <elcoosp@gmail.com>
2018-09-04 22:50:37 +0200

    refactor: 💡 Add styled-comps/polished/themmer and provide theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React, { Fragment } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
 import { Routes } from "../constants"
+import theme from "../theme"
 import { Programs, Exercises, Home } from "./screens"
 import { Header } from "./dumbs"
 
 const App = () => {
 	return (
-		<Router>
-			<Fragment>
-				<Header />
-				<Route exact path={Routes.home.path} component={Home} />
-				<Route path={Routes.exercises.path} component={Exercises} />
-				<Route path={Routes.programs.path} component={Programs} />
-			</Fragment>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Fragment>
+					<Header />
+					<Route exact path={Routes.home.path} component={Home} />
+					<Route path={Routes.exercises.path} component={Exercises} />
+					<Route path={Routes.programs.path} component={Programs} />
+				</Fragment>
+			</Router>
+		</ThemeProvider>
 	)
 }
 
---
+++ src/theme.js
@@ -0,0 +1,80 @@
+import { transparentize, lighten } from "polished"
+/*
+pm: primary
+sd: secondary
+*/
+const color = {
+	white: "#F2F2F7",
+	black: "#080C08",
+	pm: "#247BA0",
+	sd: "#0A2463"
+}
+
+const GRADIENT_DIRECTION = "to left bottom"
+
+const makeLinearGradient = color =>
+	`linear-gradient(${GRADIENT_DIRECTION}, ${color}, ${lighten(0.1)(color)})`
+
+const gradient = {
+	pm: makeLinearGradient(color.pm),
+	sd: makeLinearGradient(color.sd)
+}
+
+// /*
+// sm: small
+// md: medium
+// lg: large
+// */
+const lowOpacity = transparentize(0.7)
+const shadow = {
+	sm: `0px 1px 5px ${lowOpacity(color.black)}`,
+	md: `0px 3px 9px ${lowOpacity(color.black)}`,
+	lg: `0px 14px 40px ${lowOpacity(color.black)}`
+}
+const spacing = {
+	xs: ".7rem",
+	sm: "1rem",
+	md: "1.4rem",
+	lg: "2.2rem",
+	xl: "3rem"
+}
+
+const easing = "ease-in-out"
+const transition = {
+	sm: `all .3s ${easing}`,
+	md: `all .5s ${easing}`,
+	lg: `all .7s ${easing}`
+}
+
+const radii = {
+	sm: "6px",
+	md: "12px",
+	lg: "24px",
+	rounded: "50%"
+}
+const border = {
+	sm: `1px solid ${color.pm}`,
+	md: `2px solid ${color.pm}`,
+	lg: `4px solid ${color.pm}`
+}
+const font = {
+	size: {
+		sm: "1rem",
+		md: "1.2rem",
+		lg: "1.6rem",
+		xl: "2.4rem"
+	}
+}
+
+const theme = {
+	color,
+	border,
+	radii,
+	gradient,
+	shadow,
+	spacing,
+	font,
+	transition
+}
+
+export default theme
