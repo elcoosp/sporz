5bb66ecbcbdd116a52a37eac98277fe6b37b93f9
Louis Cospain <elcoosp@gmail.com>
2018-09-07 18:56:43 +0200

    fix: 🐛 Styling of the form

--- src/components/style/Form.js
+++ src/components/style/Form.js
@@ -1,10 +1,12 @@
 import styled from "styled-components"
+import tm from "themmer"
 const Form = styled.form`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
+	justify-content: flex-start;
 	min-height: 10rem;
+	padding: ${tm`spacing.md`};
 `
 
 export default Form
